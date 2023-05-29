8f4e8018edf3af0f90242258012206f4bbd03c44
Luis <hola@luiscaballe.com>
2023-05-29 23:40:41 +0200

    Add NavBarStyled

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,24 +1,31 @@
 import { NavLink } from "react-router-dom";
+import NavBarStyled from "./NavBarStyled";
 
 const NavBar = (): React.ReactElement => {
   return (
-    <ul>
-      <li>
-        <NavLink to="/recipes" aria-label="recipes">
-          Home
-        </NavLink>
-      </li>
-      <li>
-        <NavLink to="/add-recipe" aria-label="add recipe">
-          Add
-        </NavLink>
-      </li>
-      <li>
-        <NavLink to="/logout" aria-label="logout">
-          Logout
-        </NavLink>
-      </li>
-    </ul>
+    <NavBarStyled className="navbar">
+      <ul className="navbar__list">
+        <li>
+          <NavLink className="navbar__link" to="/recipes" aria-label="recipes">
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            className="navbar__link"
+            to="/add-recipe"
+            aria-label="add recipe"
+          >
+            Add
+          </NavLink>
+        </li>
+        <li>
+          <NavLink className="navbar__link" to="/logout" aria-label="logout">
+            Logout
+          </NavLink>
+        </li>
+      </ul>
+    </NavBarStyled>
   );
 };
 
