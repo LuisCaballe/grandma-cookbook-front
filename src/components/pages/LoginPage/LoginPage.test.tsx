08332bdf1d9604fa443133452743343e17d036bc
Luis <hola@luiscaballe.com>
2023-05-30 16:30:11 +0200

    Test loginUser

--- src/components/pages/LoginPage/LoginPage.test.tsx
+++ src/components/pages/LoginPage/LoginPage.test.tsx
@@ -1,5 +1,5 @@
 import { screen } from "@testing-library/react";
-import { renderWithProviders } from "../../testUtils/testUtils";
+import { renderWithProviders } from "../../../testUtils/testUtils";
 import LoginPage from "./LoginPage";
 
 describe("Given a LoginPage page", () => {
---
+++ src/mocks/userMocks.ts
@@ -0,0 +1,21 @@
+import { UserTokenData, UserTokenStructure } from "../store/types";
+
+export const mockCurrentState: UserTokenData = {
+  id: "",
+  name: "",
+  token: "",
+  isLogged: false,
+};
+
+export const mockUserToken: UserTokenStructure = {
+  id: "1234",
+  name: "Luis",
+  token: "mock-token",
+};
+
+export const mockExpectedNewState: UserTokenData = {
+  id: "1234",
+  name: "Luis",
+  token: "mock-token",
+  isLogged: true,
+};
