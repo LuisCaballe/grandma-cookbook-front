c23e2900aac0339f537e1af3276827083d67a942
Luis <hola@luiscaballe.com>
2023-05-30 19:34:59 +0200

    Render NavBar inside Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,13 +1,18 @@
+import NavBar from "../NavBar/NavBar";
+
 const Header = (): React.ReactElement => {
   return (
-    <header>
-      <img
-        src="images/logo.svg"
-        alt="Grandma's Cookbook's logo"
-        width="200"
-        height="63"
-      />
-    </header>
+    <>
+      <header>
+        <img
+          src="images/logo.svg"
+          alt="Grandma's Cookbook's logo"
+          width="200"
+          height="63"
+        />
+      </header>
+      <NavBar />
+    </>
   );
 };
 
