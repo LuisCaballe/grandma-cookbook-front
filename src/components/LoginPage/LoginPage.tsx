ac2f10ae402e886734dc3e57a70a2824e74bf4dd
Luis <hola@luiscaballe.com>
2023-05-30 11:58:27 +0200

    Add LoginPage

---
+++ src/components/LoginPage/LoginPage.tsx
@@ -0,0 +1,18 @@
+const LoginPage = (): React.ReactElement => {
+  return (
+    <>
+      <p>
+        Organize and relive the flavors of your childhood. Keep your culinary
+        heritage alive for generations to come.
+      </p>
+      <img
+        src="images/draw.webp"
+        alt="Illustration fo cooking ingredients"
+        width="260"
+        height="135"
+      />
+    </>
+  );
+};
+
+export default LoginPage;
