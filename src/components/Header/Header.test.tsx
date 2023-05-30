cb369122814a2346516437d24ad47e8e77f97b6f
Luis <hola@luiscaballe.com>
2023-05-30 09:38:24 +0200

    Refactor tests with renderWithProviders

--- src/components/Header/Header.test.tsx
+++ src/components/Header/Header.test.tsx
@@ -5,7 +5,9 @@ describe("Given a Header component", () => {
   describe("When it is rendered", () => {
     test("Then it should show an image with an alternative text 'Grandma's Cookbook's logo'", () => {
       const expectedAlternativeText = "Grandma's Cookbook's logo";
+
       render(<Header />);
+
       const image = screen.getByRole("img", { name: expectedAlternativeText });
 
       expect(image).toBeInTheDocument();
--- src/components/NavBar/NavBar.test.tsx
+++ src/components/NavBar/NavBar.test.tsx
@@ -1,27 +1,13 @@
-import { render, screen } from "@testing-library/react";
-
+import { screen } from "@testing-library/react";
 import NavBar from "./NavBar";
-import {
-  RouteObject,
-  RouterProvider,
-  createMemoryRouter,
-} from "react-router-dom";
-import { ThemeProvider } from "styled-components";
-import theme from "../../styles/theme/theme";
+import { renderWithProviders } from "../../testUtils/testUtils";
 
 describe("Given a NavBar component", () => {
   describe("When it is rendered", () => {
     test("Then it should show a link to 'home'", () => {
       const expectedLinkText = "home";
 
-      const route: RouteObject[] = [{ path: "/", element: <NavBar /> }];
-      const router = createMemoryRouter(route);
-
-      render(
-        <ThemeProvider theme={theme}>
-          <RouterProvider router={router} />
-        </ThemeProvider>
-      );
+      renderWithProviders(<NavBar />);
 
       const homeLink = screen.getByRole("link", { name: expectedLinkText });
 
