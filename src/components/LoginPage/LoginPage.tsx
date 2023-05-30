721cbdd9352b81d63eb9962ef308095d0445b030
Luis <hola@luiscaballe.com>
2023-05-30 12:12:01 +0200

    Add LoginPageStyled

--- src/components/LoginPage/LoginPage.tsx
+++ src/components/LoginPage/LoginPage.tsx
@@ -1,17 +1,19 @@
+import LoginPageStyled from "./LoginPageStyled";
+
 const LoginPage = (): React.ReactElement => {
   return (
-    <>
+    <LoginPageStyled>
       <p>
         Organize and relive the flavors of your childhood. Keep your culinary
         heritage alive for generations to come.
       </p>
       <img
         src="images/draw.webp"
-        alt="Illustration fo cooking ingredients"
+        alt="Illustration of cooking ingredients"
         width="260"
         height="135"
       />
-    </>
+    </LoginPageStyled>
   );
 };
 
