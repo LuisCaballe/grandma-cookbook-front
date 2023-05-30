0e839aff8e75fe226d0a17f4a11c665a1b751671
Luis <hola@luiscaballe.com>
2023-05-30 10:09:47 +0200

    Add Layout component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,16 +1,7 @@
-import Header from "../Header/Header";
-import NavBar from "../NavBar/NavBar";
-import ContainerStyled from "../shared/ContainerStyled";
+import Layout from "../Layout/Layout";
 
 const App = (): React.ReactElement => {
-  return (
-    <>
-      <ContainerStyled>
-        <Header />
-        <NavBar />
-      </ContainerStyled>
-    </>
-  );
+  return <Layout />;
 };
 
 export default App;
