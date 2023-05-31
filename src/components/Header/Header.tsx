9cefcea91c1556ae8dca328968a0e05c9310cacd
Luis <hola@luiscaballe.com>
2023-05-31 17:18:58 +0200

    Add HeaderStyled

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,19 +1,20 @@
 import { useLocation } from "react-router-dom";
 import NavBar from "../NavBar/NavBar";
+import HeaderStyled from "./HeaderStyled";
 
 const Header = (): React.ReactElement => {
   const { pathname } = useLocation();
 
   return (
     <>
-      <header>
+      <HeaderStyled>
         <img
           src="images/logo.svg"
           alt="Grandma's Cookbook's logo"
           width="200"
           height="63"
         />
-      </header>
+      </HeaderStyled>
       {pathname !== "/login" && <NavBar />}
     </>
   );
