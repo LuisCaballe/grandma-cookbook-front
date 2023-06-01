08dd04e122740254cb68ed00ab44ebd7ca2a3e60
Luis <hola@luiscaballe.com>
2023-06-01 15:37:27 +0200

    Test loginOnSubmit in LoginPage

--- src/components/pages/LoginPage/LoginPage.test.tsx
+++ src/components/pages/LoginPage/LoginPage.test.tsx
@@ -1,6 +1,17 @@
-import { screen } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { renderWithProviders } from "../../../testUtils/testUtils";
+import userEvent from "@testing-library/user-event";
 import LoginPage from "./LoginPage";
+import {
+  RouteObject,
+  RouterProvider,
+  createMemoryRouter,
+} from "react-router-dom";
+import { ThemeProvider } from "styled-components";
+import { Provider } from "react-redux";
+import { store } from "../../../store";
+import theme from "../../../styles/theme/theme";
+import { UserDataCredentials } from "../../../types";
 
 describe("Given a LoginPage page", () => {
   describe("When it is rendered", () => {
@@ -14,5 +25,41 @@ describe("Given a LoginPage page", () => {
     const image = screen.getByRole("img", { name: expectedAlternativeText });
 
     expect(image).toBeInTheDocument();
+  });
+
+  describe("When the user submit the form with a valid credentials", () => {
+    test("Then it should redirect the user to the home page", async () => {
+      const mockUser: UserDataCredentials = {
+        username: "admin",
+        password: "admin",
+      };
+      const routes: RouteObject[] = [
+        {
+          path: "/",
+          element: <LoginPage />,
+        },
+      ];
+      const router = createMemoryRouter(routes);
+      const usernameLabelText = "Username :";
+      const passwordLabelText = "Password :";
+
+      render(
+        <ThemeProvider theme={theme}>
+          <Provider store={store}>
+            <RouterProvider router={router} />
+          </Provider>
+        </ThemeProvider>
+      );
+
+      const usernameTextField = screen.getByLabelText(usernameLabelText);
+      const passwordTextField = screen.getByLabelText(passwordLabelText);
+      const button = screen.getByRole("button", { name: "Login" });
+
+      await userEvent.type(usernameTextField, mockUser.username);
+      await userEvent.type(passwordTextField, mockUser.password);
+      await userEvent.click(button);
+
+      expect(router.state.location.pathname).toBe("/home");
+    });
   });
 });
