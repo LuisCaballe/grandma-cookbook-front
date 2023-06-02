2dd5538c9a73a98d753b410f233de6cceea2d20d
Luis <hola@luiscaballe.com>
2023-06-02 16:14:06 +0200

    Add RecipesPageStyled

--- src/pages/RecipesPage/RecipesPage.tsx
+++ src/pages/RecipesPage/RecipesPage.tsx
@@ -1,12 +1,14 @@
 import { useAppSelector } from "../../store";
+import RecipesPageStyled from "./RecipesPageStyled";
 
 const RecipesPage = (): React.ReactElement => {
   const userName = useAppSelector((state) => state.user.name);
 
   return (
-    <>
-      <h1>{`${userName}'s recipes`}</h1>
-    </>
+    <RecipesPageStyled className="recipes">
+      <h1 className="recipes__title">{`${userName}'s recipes`}</h1>
+      <p>Here is your list of recipes, enjoy your meal!</p>
+    </RecipesPageStyled>
   );
 };
 
---
+++ src/pages/RecipesPage/RecipesPageStyled.ts
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+const RecipesPageStyled = styled.section`
+  display: flex;
+  flex-direction: column;
+  gap: 30px;
+
+  .recipes {
+    &__title {
+      font-size: 2rem;
+      color: ${(props) => props.theme.colors.primary};
+      text-align: center;
+      background-color: ${(props) => props.theme.colors.lightBackground};
+      padding: 10px 30px;
+    }
+  }
+`;
+
+export default RecipesPageStyled;
