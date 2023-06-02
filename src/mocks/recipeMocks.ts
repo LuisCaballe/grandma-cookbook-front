e8c018d0f000ce668e504ec86ae1c907a456f604
Luis <hola@luiscaballe.com>
2023-06-02 19:49:39 +0200

    Add dispatch loadRecipes in RecipesPage

--- src/mocks/recipeMocks.ts
+++ src/mocks/recipeMocks.ts
@@ -4,7 +4,8 @@ export const mockRecipesList: RecipeStructure[] = [
   {
     id: "647100635a615cd9e3388cab",
     name: "Fried egg",
-    imageUrl: "image1",
+    imageUrl:
+      "https://i.blogs.es/b9b3c5/huevos-fritos-en-abundante-aceite/1366_2000.jpg",
     cookingTime: 10,
     difficulty: "Easy",
     directions: "Just fry a egg",
@@ -14,7 +15,8 @@ export const mockRecipesList: RecipeStructure[] = [
   {
     id: "647102745a615cd9e3388cae",
     name: "Boiled egg",
-    imageUrl: "image1",
+    imageUrl:
+      "https://cdn.elcocinerocasero.com/imagen/receta/1000/2021-06-17-12-54-08/como-cocer-huevos.jpeg",
     cookingTime: 10,
     difficulty: "Easy",
     directions: "Just boil a egg",
