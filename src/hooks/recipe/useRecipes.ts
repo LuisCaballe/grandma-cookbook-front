58ee0ba24ed72eba9ac3ef6603091a97f7b87ab9
Luis <hola@luiscaballe.com>
2023-06-03 23:26:32 +0200

    Add getRecipes (get recipes from the API) in RecipesPage

--- src/hooks/recipe/useRecipes.ts
+++ src/hooks/recipe/useRecipes.ts
@@ -2,22 +2,22 @@ import axios from "axios";
 import { RecipeStructure, RecipesState } from "../../store/recipe/types";
 import { useAppSelector } from "../../store";
 import { apiUrl } from "../user/useUser";
+import { useCallback } from "react";
 
 const useRecipes = () => {
   const token = useAppSelector((state) => state.user.token);
 
-  const request = {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  };
-
-  const getRecipes = async (): Promise<RecipeStructure[]> => {
+  const getRecipes = useCallback(async (): Promise<RecipeStructure[]> => {
+    const request = {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    };
     const {
       data: { recipes },
     } = await axios.get<RecipesState>(`${apiUrl}/recipes`, request);
     return recipes;
-  };
+  }, [token]);
 
   return { getRecipes };
 };
--- src/pages/RecipesPage/RecipesPage.tsx
+++ src/pages/RecipesPage/RecipesPage.tsx
@@ -2,18 +2,20 @@ import { useEffect } from "react";
 import { useAppDispatch, useAppSelector } from "../../store";
 import { loadRecipesActionCreator } from "../../store/recipe/recipeSlice";
 import RecipesPageStyled from "./RecipesPageStyled";
-import { getRecipesListMock } from "../../factories/recipe/recipeFactory";
 import RecipesList from "../../components/RecipesList/RecipesList";
-
-const mockRecipesList = getRecipesListMock(4);
+import useRecipes from "../../hooks/recipe/useRecipes";
 
 const RecipesPage = (): React.ReactElement => {
   const dispatch = useAppDispatch();
   const userName = useAppSelector((state) => state.user.name);
+  const { getRecipes } = useRecipes();
 
   useEffect(() => {
-    dispatch(loadRecipesActionCreator(mockRecipesList));
-  });
+    (async () => {
+      const recipesList = await getRecipes();
+      dispatch(loadRecipesActionCreator(recipesList));
+    })();
+  }, [dispatch, getRecipes]);
 
   return (
     <RecipesPageStyled className="recipes">
