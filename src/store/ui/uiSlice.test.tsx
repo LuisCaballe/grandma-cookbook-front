6f3e7b7d99f9e9962b078566f898789132f637b3
Luis <hola@luiscaballe.com>
2023-06-04 20:45:31 +0200

    Test hideLoading reducer

--- src/store/ui/uiSlice.test.tsx
+++ src/store/ui/uiSlice.test.tsx
@@ -1,5 +1,9 @@
 import { uiStructure } from "./types";
-import { showLoadingActionCreator, uiReducer } from "./uiSlice";
+import {
+  hideLoadingActionCreator,
+  showLoadingActionCreator,
+  uiReducer,
+} from "./uiSlice";
 
 describe("Given a showLoading reducer", () => {
   describe("When it receives the current UI state and a showLoading action", () => {
@@ -8,6 +12,19 @@ describe("Given a showLoading reducer", () => {
       const expectedUiState: uiStructure = { isLoading: true };
 
       const newUiState = uiReducer(currentUiState, showLoadingActionCreator());
+
+      expect(newUiState).toStrictEqual(expectedUiState);
+    });
+  });
+});
+
+describe("Given a hideLoading reducer", () => {
+  describe("When it receives the current UI state and a hideLoading action", () => {
+    test("Then it should return the new UI state with isLoading property set at false", () => {
+      const currentUiState: uiStructure = { isLoading: true };
+      const expectedUiState: uiStructure = { isLoading: false };
+
+      const newUiState = uiReducer(currentUiState, hideLoadingActionCreator());
 
       expect(newUiState).toStrictEqual(expectedUiState);
     });
--- src/store/ui/uiSlice.ts
+++ src/store/ui/uiSlice.ts
@@ -21,5 +21,9 @@ const uiSlice = createSlice({
   },
 });
 
-export const { showLoading: showLoadingActionCreator } = uiSlice.actions;
+export const {
+  showLoading: showLoadingActionCreator,
+  hideLoading: hideLoadingActionCreator,
+} = uiSlice.actions;
+
 export const uiReducer = uiSlice.reducer;
