2bd54a5222b4c345322f51c23bb342b2c5c9e7c4
Luis <hola@luiscaballe.com>
2023-06-04 16:02:33 +0200

    Add RecipesPage as lazyPage in appRouter

--- src/routers/LazyPages.tsx
+++ src/routers/LazyPages.tsx
@@ -1,3 +1,6 @@
 import { lazy } from "react";
 
 export const LazyLoginPage = lazy(() => import("../pages/LoginPage/LoginPage"));
+export const LazyRecipesPage = lazy(
+  () => import("../pages/RecipesPage/RecipesPage")
+);
