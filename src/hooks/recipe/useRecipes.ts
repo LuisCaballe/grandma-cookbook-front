0f4f438283c490a55a564bc315e329964586bfa4
Luis <hola@luiscaballe.com>
2023-06-04 21:48:36 +0200

    Add Loader in Layout

--- src/hooks/recipe/useRecipes.ts
+++ src/hooks/recipe/useRecipes.ts
@@ -1,13 +1,20 @@
 import axios from "axios";
 import { RecipeStructure, RecipesState } from "../../store/recipe/types";
-import { useAppSelector } from "../../store";
+import { useAppDispatch, useAppSelector } from "../../store";
 import { apiUrl } from "../user/useUser";
 import { useCallback } from "react";
+import {
+  hideLoadingActionCreator,
+  showLoadingActionCreator,
+} from "../../store/ui/uiSlice";
 
 const useRecipes = () => {
   const token = useAppSelector((state) => state.user.token);
+  const dispatch = useAppDispatch();
 
   const getRecipes = useCallback(async (): Promise<RecipeStructure[]> => {
+    dispatch(showLoadingActionCreator());
+
     const request = {
       headers: {
         Authorization: `Bearer ${token}`,
@@ -16,8 +23,11 @@ const useRecipes = () => {
     const {
       data: { recipes },
     } = await axios.get<RecipesState>(`${apiUrl}/recipes`, request);
+
+    dispatch(hideLoadingActionCreator());
+
     return recipes;
-  }, [token]);
+  }, [dispatch, token]);
 
   return { getRecipes };
 };
