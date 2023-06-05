70dac6dedd31e664e511855ae490b6f73b44a2cf
Luis <hola@luiscaballe.com>
2023-06-05 12:38:36 +0200

    Refactor rendering Loader

--- src/hooks/recipe/useRecipes.ts
+++ src/hooks/recipe/useRecipes.ts
@@ -3,18 +3,13 @@ import { RecipeStructure, RecipesState } from "../../store/recipe/types";
 import { useAppDispatch, useAppSelector } from "../../store";
 import { apiUrl } from "../user/useUser";
 import { useCallback } from "react";
-import {
-  hideLoadingActionCreator,
-  showLoadingActionCreator,
-} from "../../store/ui/uiSlice";
+import { hideLoadingActionCreator } from "../../store/ui/uiSlice";
 
 const useRecipes = () => {
   const token = useAppSelector((state) => state.user.token);
   const dispatch = useAppDispatch();
 
   const getRecipes = useCallback(async (): Promise<RecipeStructure[]> => {
-    dispatch(showLoadingActionCreator());
-
     const request = {
       headers: {
         Authorization: `Bearer ${token}`,
