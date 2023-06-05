fbde535c57b724ea33993f525dde738ae6b89e1d
Luis <hola@luiscaballe.com>
2023-06-05 19:57:50 +0200

    Test onClick function in Feedback component

--- src/components/Feedback/Feedback.test.tsx
+++ src/components/Feedback/Feedback.test.tsx
@@ -1,6 +1,8 @@
 import Feedback from "./Feedback";
 import { renderWithProviders } from "../../testUtils/testUtils";
 import { screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { store } from "../../store";
 
 describe("Given a Feedback component", () => {
   describe("When it is rendered", () => {
@@ -11,6 +13,22 @@ describe("Given a Feedback component", () => {
       const feedBackBox = screen.getByLabelText(expectedLabelText);
 
       expect(feedBackBox).toBeInTheDocument();
+    });
+
+    test("Then it should show a button and when the user clicks on it, the message of the UI state should set to empty ", async () => {
+      const textButton = "Close";
+
+      renderWithProviders(<Feedback />, {
+        ui: { isLoading: false, isError: true, message: "Wrong credentials" },
+      });
+
+      const closeButton = screen.getByRole("button", { name: textButton });
+
+      await userEvent.click(closeButton);
+
+      const testStore = store.getState();
+
+      expect(testStore.ui.message).toBeFalsy();
     });
   });
 });
