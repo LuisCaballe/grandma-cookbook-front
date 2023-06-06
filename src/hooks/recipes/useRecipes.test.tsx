a00a6818f7d2f5dcf11c7055148af401ecbc11a0
Luis <hola@luiscaballe.com>
2023-06-06 20:01:02 +0200

    Refactor useRecipes test

--- src/hooks/recipes/useRecipes.test.tsx
+++ src/hooks/recipes/useRecipes.test.tsx
@@ -1,9 +1,18 @@
-import { renderHook } from "@testing-library/react";
+import { renderHook, screen } from "@testing-library/react";
 import useRecipes from "./useRecipes";
 import { mockRecipesList } from "../../mocks/recipeMocks";
-import { wrapperWithProvider } from "../../testUtils/testUtils";
+import {
+  renderWithProviders,
+  wrapperWithProvider,
+} from "../../testUtils/testUtils";
 import { server } from "../../mocks/server";
 import { errorHandlers } from "../../mocks/handlers";
+import {
+  RouteObject,
+  RouterProvider,
+  createMemoryRouter,
+} from "react-router-dom";
+import App from "../../components/App/App";
 
 describe("Given a getRecipes function", () => {
   describe("When it is called with a valid token", () => {
@@ -21,18 +30,22 @@ describe("Given a getRecipes function", () => {
   });
 
   describe("When it receives a invalid token", () => {
-    test("Then it should resolve undefined", () => {
+    test("Then it should show a modal with a icon with the alternative text 'error icon'", async () => {
       server.resetHandlers(...errorHandlers);
-
+      const expectedAltText = "error icon";
       const {
         result: {
           current: { getRecipes },
         },
       } = renderHook(() => useRecipes(), { wrapper: wrapperWithProvider });
+      const routes: RouteObject[] = [{ path: "/", element: <App /> }];
+      const router = createMemoryRouter(routes);
 
-      const getRecipesFunction = getRecipes();
+      renderWithProviders(<RouterProvider router={router} />);
+      await getRecipes();
+      const icon = screen.getByAltText(expectedAltText);
 
-      expect(getRecipesFunction).resolves.toBeUndefined();
+      expect(icon).toBeInTheDocument();
     });
   });
 });
