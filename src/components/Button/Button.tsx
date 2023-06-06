55104c4291f70aa3ff347b77d88b8494ed6d34a3
Luis <hola@luiscaballe.com>
2023-06-06 16:39:09 +0200

    Add Button component in LoginForm

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -6,6 +6,8 @@ interface ButtonProps {
   altText?: string;
   width?: string;
   height?: string;
+  isDisabled?: boolean;
+  type?: "submit" | "reset" | "button" | undefined;
 }
 
 const Button = ({
@@ -16,9 +18,16 @@ const Button = ({
   altText,
   width,
   height,
+  isDisabled,
+  type,
 }: ButtonProps): React.ReactElement => {
   return (
-    <button className={className} onClick={actionOnClick}>
+    <button
+      className={className}
+      onClick={actionOnClick}
+      disabled={isDisabled}
+      type={type}
+    >
       {text ||
         (icon && (
           <img src={icon} alt={altText} width={width} height={height} />
