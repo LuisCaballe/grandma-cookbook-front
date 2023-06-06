62268f630e7d243673f943b5a6b37b38e44a3a30
Luis <hola@luiscaballe.com>
2023-06-06 11:02:26 +0200

    Add feedback error in getRecipes

--- src/hooks/recipes/useRecipes.ts
+++ src/hooks/recipes/useRecipes.ts
@@ -3,25 +3,41 @@ import { RecipeStructure, RecipesState } from "../../store/recipe/types";
 import { useAppDispatch, useAppSelector } from "../../store";
 import { apiUrl } from "../user/useUser";
 import { useCallback } from "react";
-import { hideLoadingActionCreator } from "../../store/ui/uiSlice";
+import {
+  hideLoadingActionCreator,
+  showFeedbackActionCreator,
+} from "../../store/ui/uiSlice";
 
 const useRecipes = () => {
   const token = useAppSelector((state) => state.user.token);
   const dispatch = useAppDispatch();
 
-  const getRecipes = useCallback(async (): Promise<RecipeStructure[]> => {
-    const request = {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    };
-    const {
-      data: { recipes },
-    } = await axios.get<RecipesState>(`${apiUrl}/recipes`, request);
+  const getRecipes = useCallback(async (): Promise<
+    RecipeStructure[] | undefined
+  > => {
+    try {
+      const request = {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      };
+      const {
+        data: { recipes },
+      } = await axios.get<RecipesState>(`${apiUrl}/recipes`, request);
 
-    dispatch(hideLoadingActionCreator());
+      dispatch(hideLoadingActionCreator());
 
-    return recipes;
+      return recipes;
+    } catch (error) {
+      const errorMessage = "Recipes couldn't be load. Please, try again";
+      dispatch(
+        showFeedbackActionCreator({
+          message: errorMessage,
+          isError: true,
+        })
+      );
+      dispatch(hideLoadingActionCreator());
+    }
   }, [dispatch, token]);
 
   return { getRecipes };
