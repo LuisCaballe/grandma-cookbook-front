6b8faab5cfed07b4714998a387c2d2f3a819c758
Luis <hola@luiscaballe.com>
2023-06-07 18:57:37 +0200

    Refactor LoginPage test

--- src/hooks/recipes/useRecipes.ts
+++ src/hooks/recipes/useRecipes.ts
@@ -2,25 +2,30 @@ import axios from "axios";
 import { RecipeStructure, RecipesState } from "../../store/recipe/types";
 import { useAppDispatch, useAppSelector } from "../../store";
 import { apiUrl } from "../user/useUser";
-import { useCallback } from "react";
+import { useCallback, useMemo } from "react";
 import {
   hideLoadingActionCreator,
   showFeedbackActionCreator,
+  showLoadingActionCreator,
 } from "../../store/ui/uiSlice";
 
 const useRecipes = () => {
   const token = useAppSelector((state) => state.user.token);
   const dispatch = useAppDispatch();
 
+  const request = useMemo(
+    () => ({
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }),
+    [token]
+  );
+
   const getRecipes = useCallback(async (): Promise<
     RecipeStructure[] | undefined
   > => {
     try {
-      const request = {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      };
       const {
         data: { recipes },
       } = await axios.get<RecipesState>(`${apiUrl}/recipes`, request);
@@ -38,9 +43,14 @@ const useRecipes = () => {
       );
       dispatch(hideLoadingActionCreator());
     }
-  }, [dispatch, token]);
+  }, [dispatch, request]);
 
-  return { getRecipes };
+  const removeRecipe = (recipeId: string) => {
+    dispatch(showLoadingActionCreator());
+    axios.delete(`${apiUrl}/recipes${recipeId}`, request);
+  };
+
+  return { getRecipes, removeRecipe };
 };
 
 export default useRecipes;
