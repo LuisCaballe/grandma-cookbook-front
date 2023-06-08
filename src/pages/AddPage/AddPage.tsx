5fb6db4477940fbab47b70c9655134a29c920611
Luis <hola@luiscaballe.com>
2023-06-08 15:51:29 +0200

    Test AddPage

--- src/pages/AddPage/AddPage.tsx
+++ src/pages/AddPage/AddPage.tsx
@@ -4,7 +4,7 @@ const AddPage = (): React.ReactElement => {
   return (
     <AddPageStyled>
       <section className="add">
-        <h1 className="add__title">Add recipes</h1>
+        <h1 className="add__title">Add recipe</h1>
         <p>
           Fill in the form with your delicious recipe and add it to your
           collection.
