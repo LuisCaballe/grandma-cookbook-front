3867c0fdb1fd763b7c8b7c0bca1a3f640abf49bf
Luis <hola@luiscaballe.com>
2023-06-08 21:14:12 +0200

    Add state variable in Form component

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+import { RecipeStructure } from "../../store/recipe/types";
 import Button from "../Button/Button";
 import FormStyled from "../shared/FromStyled";
 
@@ -6,42 +8,94 @@ interface FormProps {
 }
 
 const Form = ({ buttonText }: FormProps): React.ReactElement => {
+  const initialReceivedRecipeData: Partial<RecipeStructure> = {
+    name: "",
+    imageUrl: "",
+    difficulty: "",
+    cookingTime: 0,
+    ingredients: "",
+    directions: "",
+  };
+
+  const [receivedRecipeData, setReceivedRecipeData] = useState(
+    initialReceivedRecipeData
+  );
+
+  const onChangeData = (
+    event: React.ChangeEvent<
+      HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement
+    >
+  ) => {
+    setReceivedRecipeData({
+      ...receivedRecipeData,
+      [event.target.id]: event.target.value,
+    });
+  };
+
   return (
-    <FormStyled className="form">
+    <FormStyled className="form" autoComplete="off">
       <div className="form__control">
         <label htmlFor="name" className="form__label">
           Name :
         </label>
-        <input id="name" type="text" className="form__input" />
+        <input
+          id="name"
+          type="text"
+          className="form__input"
+          onChange={onChangeData}
+          value={receivedRecipeData.name}
+        />
       </div>
       <div className="form__control">
         <label htmlFor="imageUrl" className="form__label">
           Image url :
         </label>
-        <input id="imageUrl" type="text" className="form__input" />
+        <input
+          id="imageUrl"
+          type="text"
+          className="form__input"
+          onChange={onChangeData}
+          value={receivedRecipeData.imageUrl}
+        />
       </div>
       <div className="form__control">
         <label htmlFor="difficulty" className="form__label">
           Difficulty :
         </label>
-        <select id="difficulty" className="form__input">
-          <option>-- Choose difficulty --</option>
-          <option>Easy</option>
-          <option>Moderate</option>
-          <option>Advanced</option>
+        <select
+          id="difficulty"
+          className="form__input"
+          onChange={onChangeData}
+          value={receivedRecipeData.difficulty}
+        >
+          <option value="">-- Choose difficulty --</option>
+          <option value="Easy">Easy</option>
+          <option value="Moderate">Moderate</option>
+          <option value="Advanced">Advanced</option>
         </select>
       </div>
       <div className="form__control">
         <label htmlFor="cookingTime" className="form__label">
           Cooking time (minutes) :
         </label>
-        <input id="cookingTime" type="number" className="form__input" />
+        <input
+          id="cookingTime"
+          type="number"
+          className="form__input"
+          onChange={onChangeData}
+          value={receivedRecipeData.cookingTime || ""}
+        />
       </div>
       <div className="form__control">
-        <label htmlFor="ingredientes" className="form__label">
+        <label htmlFor="ingredients" className="form__label">
           Ingredients :
         </label>
-        <textarea id="ingredientes" className="form__textarea" />
+        <textarea
+          id="ingredients"
+          className="form__textarea"
+          onChange={onChangeData}
+          value={receivedRecipeData.ingredients}
+        />
       </div>
       <div className="form__control">
         <label htmlFor="directions" className="form__label">
@@ -50,6 +104,8 @@ const Form = ({ buttonText }: FormProps): React.ReactElement => {
         <textarea
           id="directions"
           className="form__textarea form__textarea--big"
+          onChange={onChangeData}
+          value={receivedRecipeData.directions}
         />
       </div>
       <Button type="submit" className="form__button" text={buttonText} />
