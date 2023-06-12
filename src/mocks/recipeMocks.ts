b76e45206a90ecb5cb081e72f899d8efc8957a98
Luis <hola@luiscaballe.com>
2023-06-12 18:21:51 +0200

    Refactor pagination functionality test in RecipesPage

--- src/mocks/recipeMocks.ts
+++ src/mocks/recipeMocks.ts
@@ -24,39 +24,3 @@ export const mockRecipesList: RecipeStructure[] = [
     user: "646fa0775a615cd9e3388ca9",
   },
 ];
-
-export const mockThreeRecipes: RecipeStructure[] = [
-  {
-    id: "647102745a615cd9e3388caf",
-    name: "Spanish Omelette",
-    imageUrl:
-      "https://cdn.elcocinerocasero.com/imagen/receta/1000/2021-06-17-12-54-08/como-cocer-huevos.jpeg",
-    cookingTime: 10,
-    difficulty: "Easy",
-    directions: "Just do it",
-    ingredients: "Potatoes, egg",
-    user: "646fa0775a615cd9e3388ca9",
-  },
-  {
-    id: "647100635a615cd9e3388cab",
-    name: "Fried egg",
-    imageUrl:
-      "https://i.blogs.es/b9b3c5/huevos-fritos-en-abundante-aceite/1366_2000.jpg",
-    cookingTime: 10,
-    difficulty: "Easy",
-    directions: "Just fry a egg",
-    ingredients: "Oil, egg",
-    user: "646fa0775a615cd9e3388ca9",
-  },
-  {
-    id: "647102745a615cd9e3388cae",
-    name: "Boiled egg",
-    imageUrl:
-      "https://cdn.elcocinerocasero.com/imagen/receta/1000/2021-06-17-12-54-08/como-cocer-huevos.jpeg",
-    cookingTime: 10,
-    difficulty: "Easy",
-    directions: "Just boil a egg",
-    ingredients: "Water, egg",
-    user: "646fa0775a615cd9e3388ca9",
-  },
-];
--- src/pages/RecipesPage/RecipesPage.test.tsx
+++ src/pages/RecipesPage/RecipesPage.test.tsx
@@ -1,12 +1,15 @@
-import { screen } from "@testing-library/react";
+import { screen, waitFor } from "@testing-library/react";
 import { renderWithProviders } from "../../testUtils/testUtils";
 import RecipesPage from "./RecipesPage";
 import { getUserMock } from "../../factories/user/userFactory";
 import { server } from "../../mocks/server";
 import { paginationHandlers } from "../../mocks/handlers";
 import userEvent from "@testing-library/user-event";
+import { vi } from "vitest";
 
 describe("Given a RecipesPage page", () => {
+  window.scrollTo = vi.fn().mockImplementation(() => ({}));
+
   describe("When it is rendered", () => {
     test("Then it should show a heading with the text 'John's recipes'", () => {
       const expectedTitleText = "John's recipes";
@@ -26,23 +29,36 @@ describe("Given a RecipesPage page", () => {
     });
   });
 
-  describe("When it is rendered and the user clicks on the button with the right arrow", () => {
-    test("Then it should show the next page", async () => {
+  describe("When it is rendered and the user clicks on the next button", () => {
+    test("Then the next button should be enabled", async () => {
       server.resetHandlers(...paginationHandlers);
 
       renderWithProviders(<RecipesPage />);
 
       const nextButton = screen.getByLabelText("next button");
+      await userEvent.click(nextButton);
+
+      waitFor(() => {
+        expect(nextButton).toBeEnabled();
+      });
+    });
+  });
+
+  describe("When it is rendered and the user clicks on the next button and after that on the previous button", () => {
+    test("Then the previous button should be enabled", async () => {
+      server.resetHandlers(...paginationHandlers);
+
+      renderWithProviders(<RecipesPage />);
+
+      const nextButton = screen.getByLabelText("next button");
+      await userEvent.click(nextButton);
 
       const previousButton = screen.getByLabelText("previous button");
+      await userEvent.click(previousButton);
 
-      expect(nextButton).toBeInTheDocument();
-      expect(previousButton).toBeInTheDocument();
-
-      await userEvent.click(nextButton);
-      await userEvent.click(nextButton);
-
-      expect(nextButton).toBeDisabled();
+      waitFor(() => {
+        expect(previousButton).toBeEnabled();
+      });
     });
   });
 });
